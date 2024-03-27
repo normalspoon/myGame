9dc265e2c943cd4ecbafc7e8bb91746a74e56c4b
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-03-27 21:59:09 +1100

    wednesday work

--- script.js
+++ script.js
@@ -2,39 +2,61 @@
 const PLAYER = "PLAYER";
 const TILE_IMAGES = "TILE_IMAGES";
 const MAX_GUESSES = "MAX_GUESSES";
+// const IMG_LIST = [
+//     https://imgur.com/t/cat/YF3CZcZ,
+//     https://imgur.com/t/cat/uOB6y3P,
+//     https://imgur.com/t/cat/bCfyCpH,
+//     https://imgur.com/t/cat/dkLt6kH,
+//     https://imgur.com/t/cat/UbJPvdn,
+//     https://imgur.com/t/cat/qIGSfwA,
+//     https://imgur.com/t/cat/Tpl3kek,
+//     https://imgur.com/t/cat/r8VcaJR
+// ]
 //const TILE_NUMBER = "TILE_NUMBER";
 
 /*----- app's state (variables) -----*/
 let playerChoice;
 let tilePositions;
-let tileState; //where 0 is face down. 1 is face up.
 let guesses;
-let flippedTiles;
+let tileProperties;
 
 /*----- cached element references -----*/
-const TILES = document.querySelectorAll("#tile");
+const TILES = document.querySelectorAll(".tile");
 const BUTTON = document.getElementById("actionButton");
+
 
 /*----- event listeners -----*/
 TILES.forEach((tile) => {
   tile.addEventListener("click", boardClickHandler);
 });
 
-// BUTTON_EL.addEventListener('click',buttonClickHandler)
-
 // /*----- model functions -----*/
-function initBoard() {
+
+
+function populateBoard() {
   tilePositions = [];
-  for (let i = 0; i < 16; i++) {
-    tilePositions.push("");
+  for (let i = 0; i < TILES.length; i++) {
+    let propertiesObjects = {};
+    properties.forEach(function (property) {
+      propertiesObjects[property] = "";
+    });
+    propertiesObjects["id"] = (i +1);
+    propertiesObjects["state"] = "0";
+    // propertiesOBjects["img"] = Math.floor(Math.random()* IMG_LIST.length)
+    tilePositions.push(propertiesObjects);
   }
-  tileState = 0;
-  guesses = 0;
-  flippedTiles = 0;
-
-  render()
+  return tilePositions;
 }
+let properties = ["state", "img", "id"];
+populateBoard()
+console.log(tilePositions);
 
+
+
+
+// tilePositions.forEach(function(tile, index){
+    
+// })
 // /*----- controller functions -----*/
 
 function boardClickHandler(evt) {
@@ -42,9 +64,7 @@ function boardClickHandler(evt) {
   if (evt.target.tagName !== "DIV") {
     return;
   }
-  console.log("I am flipped");
 }
-
 // }
 
 // function buttonClickHandler(evt) {
@@ -52,5 +72,3 @@ function boardClickHandler(evt) {
 // }
 
 // *----- view functions -----*/
-
-
