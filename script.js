652ee9500c337455b0bdef11c37bc74a184cf3f2
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-04-03 22:15:25 +1100

    managed to insert images into clicked tiles

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const PLAYER = "PLAYER";
 const TILE_IMAGES = "TILE_IMAGES";
 const MAX_GUESSES = "MAX_GUESSES";
 const IMG_LIST = [
-  "https://imgur.com/t/cat/YF3CZcZ",
-  "https://imgur.com/t/cat/uOB6y3P",
-  "https://imgur.com/t/cat/bCfyCpH",
-  "https://imgur.com/t/cat/dkLt6kH",
-  "https://imgur.com/t/cat/UbJPvdn",
-  "https://imgur.com/t/cat/qIGSfwA",
-  "https://imgur.com/t/cat/Tpl3kek",
-  "https://imgur.com/t/cat/r8VcaJR",
+  "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_1280.jpg",
+  "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+  "https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/02/15/12/12/cat-2068462_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/08/07/16/36/cat-2605502_1280.jpg",
+  "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg",
+  "https://cdn.pixabay.com/photo/2019/11/08/11/56/kitten-4611189_1280.jpg",
+  "https://cdn.pixabay.com/photo/2023/09/21/17/05/european-shorthair-8267220_1280.jpg",
 ];
 const TILE_NUMBER = "TILE_NUMBER";
 
@@ -23,6 +23,7 @@ let tileProperties;
 /*----- cached element references -----*/
 const TILES = document.querySelectorAll(".tile");
 const BUTTON = document.getElementById("actionButton");
+const guessesDisplay = document.getElementById("attempts");
 
 // /*----- model functions -----*/
 
@@ -31,7 +32,7 @@ function populateBoard() {
   for (let i = 0; i < TILES.length; i++) {
     let propertiesObject = {
       id: TILES[i].id,
-      state: "0",
+      state: "false",
       img: "",
     };
     // properties.forEach(function (property) {
@@ -67,10 +68,8 @@ function buttonClickHandler(evt) {
     imgList16.push(img);
     imgList16.push(img);
   }
-  // console.log(imgList16);
-  // shuffle(imgList16);
-  // console.log(imgList16);
 }
+
 
 function shuffle(array) {
   let currentIndex = array.length;
@@ -84,34 +83,67 @@ function shuffle(array) {
   }
 }
 
-buttonClickHandler()
-shuffle(imgList16)
-console.log(imgList16)
+buttonClickHandler();
+shuffle(imgList16);
+console.log(imgList16);
 
 
-let firstPick = 8;
+guesses = 0
 function boardClickHandler(evt) {
-  //   evt.preventDefault();
-  //   if (evt.target.tagName !== "DIV") {
-  //     return;
-  //   }
+    evt.preventDefault();
+    if (evt.target.tagName !== "DIV") {
+      return;
+    }
+  
+  function addGuessNumber() {
+  guesses += 1 
+  return guesses
+  }
+  guesses = addGuessNumber()
+  guessesDisplay.innerText = `GUESSES: ${guesses}`
+ 
   const clickedTileID = evt.target.id;
   const clickedTile = tilePositions.find((tile) => tile.id === clickedTileID);
   console.log(clickedTile);
 
-  clickedTile.state = "1";
+  clickedTile.state = "true";
+
   console.log(
     "after click TILE ID " + clickedTileID + " state changed to",
     clickedTile.state
   );
-  if (clickedTile.img !== '') {
-   return
-} else {
-  clickedTile.img = imgList16.shift()
-}
-  
-  console.log("after click Tile Img" + clickedTileID + "inserted " + clickedTile.img)
-  console.log(tilePositions)
+
+  if (clickedTile.img !== "") {
+    return;
+  } else {
+    clickedTile.img = imgList16.shift();
+  }
+
+  console.log(
+    "after click Tile Img " + clickedTileID + "inserted " + clickedTile.img
+  );
+  console.log(tilePositions);
+  console.log(tilePositions[clickedTileID - 1]);
+  console.log(clickedTile.state);
+ 
+ 
+  // *----- view functions -----*/
+tilePositions.forEach(tile => {
+  // Get the corresponding div element by ID
+  const TILE_EL = document.getElementById(tile.id);
+  // console.log(TILE_EL)
+  if (tile.img !== '') {
+      // Set the background image of the div to the image URL
+      TILE_EL.style.backgroundImage = `url(${tile.img})`;
+  }
+  if (tile.state === 'false') {
+
+  }
+
+});
+
+
+
+
 }
 
-// *----- view functions -----*/
